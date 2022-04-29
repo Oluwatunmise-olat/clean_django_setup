4787ee6b6ed85deb9f816613221f7a349ece1ad1
Oluwatunmise <oluwatunmiseolatunbosun2001@gmail.com>
2022-04-29 15:37:42 +0100

    ...

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -5,10 +5,6 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
 
