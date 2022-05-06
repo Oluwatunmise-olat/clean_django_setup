6da88f7526b44634d8767ad557f4ede6b0f5201d
Oluwatunmise <oluwatunmiseolatunbosun2001@gmail.com>
2022-05-06 20:45:17 +0100

    seen what i was curious about ... :)

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    # path("admin/", admin.site.urls),
+    path("admin/", admin.site.urls),
     path("auth/", include("auth_controller.urls", namespace="auth_routes")),
     path("posts/", include("post_controller.urls", namespace="post_routes")),
 ]
